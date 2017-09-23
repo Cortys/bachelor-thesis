1cb817cac8ae952ef4cf2d554637b64ddc843066
Clemens Damke <admin@cortys.de>
2017-09-23 20:51:48 +0200

    extended PSL example

--- thesis/svg2pdf.sh
+++ thesis/svg2pdf.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-find gfx/**/*.svg -exec bash -c 'x={}; y=`dirname $x`; z=$y/`basename $x .svg`.pdf; inkscape $x --export-pdf $z' \;
+find gfx/**/${1:-*}.svg -exec bash -c 'x={}; y=`dirname $x`; z=$y/`basename $x .svg`.pdf; inkscape $x --export-pdf $z' \;
+
+if [[ $2 = "crop" ]]; then
+	find gfx/**/${1:-*}.pdf -exec bash -c 'x={}; pdfcrop $x $x' \;
+fi
