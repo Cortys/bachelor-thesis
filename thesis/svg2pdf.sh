ea60901966515961cc74beffe986b78ad908cc00
Clemens Damke <admin@cortys.de>
2017-09-26 19:19:06 +0200

    started describing KG ontology

--- thesis/svg2pdf.sh
+++ thesis/svg2pdf.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-find gfx/**/${1:-*}.svg -exec bash -c 'x={}; y=`dirname $x`; z=$y/`basename $x .svg`.pdf; inkscape $x --export-pdf $z' \;
+find gfx/${1:-**/*}.svg -exec bash -c 'x={}; y=`dirname $x`; z=$y/`basename $x .svg`.pdf; inkscape $x --export-pdf $z' \;
 
 if [[ $2 = "crop" ]]; then
-	find gfx/**/${1:-*}.pdf -exec bash -c 'x={}; pdfcrop $x $x' \;
+	find gfx/${1:-**/*}.pdf -exec bash -c 'x={}; pdfcrop $x $x' \;
 fi
