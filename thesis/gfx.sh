259fb126d7452646e9a3239a5d224be09038eed1
Clemens Damke <admin@cortys.de>
2017-09-30 22:02:21 +0200

    started concept transformation description

--- thesis/gfx.sh
+++ thesis/gfx.sh
@@ -4,3 +4,7 @@ find gfx/${1:-**/*}.svg -exec bash -c 'x={}; y=`dirname $x`; z=$y/`basename $x .
 if [[ $2 = "crop" ]]; then
 	find gfx/${1:-**/*}.pdf -exec bash -c 'x={}; pdfcrop $x $x' \;
 fi
+
+if [[ $2 = "dot" ]]; then
+	find gfx/${1:-**/*}.dot -exec bash -c 'x={}; y=`dirname $x`; z=$y/`basename $x .dot`.pdf; dot -Tpdf $x -o $z' \;
+fi
