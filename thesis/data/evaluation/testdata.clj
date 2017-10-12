8c050e0860b82da3d945a68a4c519b4ed2e05315
Clemens Damke <admin@cortys.de>
2017-10-12 15:11:41 +0200

    started describing testdata

---
+++ thesis/data/evaluation/testdata.clj
@@ -0,0 +1,89 @@
+[; Message 1:
+ {:from "Duane Seppi", :to "Vince J Kaminski", :date "2001-03-13T09:54:24"
+  :content
+  "Vince,
+
+  Sorry that I missed your call yesterday.  I have a meeting from 2-3
+  today
+  (Tuesday), but otherwise any time in the afternoon works for me.  Let me
+  know what is convenient for you.  Thanks for your help.
+
+  Duane"}
+ ; Message 2:
+ {:from "Vince J Kaminski", :to "Duane Seppi", :date "2001-03-14T08:44"
+  :content
+  "Duane,
+
+  I shall be traveling for the rest of the week but my colleague
+  Dr. Zimin Lu will call you to talk about different
+  structures.
+
+  Vince"}
+ ; Message 3:
+ {:from "Duane Seppi", :to "Zimin Lu", :date "2001-03-14T15:15:02"
+  :content
+  "Dr. Lu,
+
+  I would be grateful if I could talk with you some time about the typical
+  terms one sees in Swing, take or pay, virtual storage, etc. options.
+  This
+  is related to some research some colleagues and I are doing applying
+  recent
+  innovations in Monte Carlo valuation of options with early exercise.  We
+  would like to illustrate our techniques on some examples which look
+  realistic.  When would be convenient for you?
+
+  I look forward to talking with you.
+  Duane Seppi"}
+ ; Message 4:
+ {:from "Zimin Lu", :to "Duane Seppi", :date "2001-03-14T15:57"
+  :content
+  "Dr. Seppi,
+
+  My phone number is 713-853-6388.  I will call you
+  tomorrow afternoon around 1:30 pm Houston time.
+
+  Zimin"}
+ ; Message 5:
+ {:from "Duane Seppi", :to "Zimin Lu", :date "2001-03-15T11:59:59"
+  :content
+  "Great.  I'll be in my office.   My number is 412-268-2298.
+
+  Duane"}
+ ; Message 6:
+ {:from "Zimin Lu", :to "Duane Seppi", :date "2001-03-15T13:58"
+  :content
+  "Dr. Seppi,
+
+  Nice talking to you about swing contracts and your recent work on
+  American-Monte
+  Carlo approach.  I am interested to read your preprint papers.
+
+  My address is
+
+  Zimin Lu
+  Enron Corp, EB 1967
+  1400 Smith Street
+  Houston, TX 77002-7361
+
+
+  Zimin"}
+ ; Message 7:
+ {:from "Duane Seppi", :to "Zimin Lu", :date "2001-03-15T21:26:00"
+  :content
+  "Zimin,
+
+  I also enjoyed our talk.  Thanks very much.  I'll send you the paper as soon
+  as its done.
+
+  Duane"}
+ ; Message 8:
+ {:from "John Doe", :to "Vince J Kaminski", :date "2017-07-07T07:07:07"
+  :content
+  "Hello!
+
+   Your conversation with Dr. Seppi unfortunately doesn't contain any negation.
+   I'm not writing this message because I have something to say,
+   I just need an example message that contains negation.
+
+   John"}]
